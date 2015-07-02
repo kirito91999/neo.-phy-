049af27358035584b72324b118823b10596f0865
Jose F. Luna <jluna80@gmail.com>
2015-07-02 11:51:18 -0430

    funciones para calculos de neobux mejor escenario posible con 250 ref gold

--- neobux.py
+++ neobux.py
@@ -41,5 +41,21 @@ def r_a_naranja (n=1,tipo_r=0,tipo_u=0):
             click = 0.0100
     return n * click
 
-            
+tipo_usuario = 1 # 0 standar 1 gold
+anuncios_n = 4 # anuncios validos naranja para el usuario
+anuncios_m = 19 # anuncios micro y morados del usuario
+media_rd = 18 # media del referido directo
+media_ra = 4 # media del referido alquilado
+referidos_a = 250 # referidos alquilados
+referidos_d = 30 # referidos directo
+costo_ra = 0.2 # referido alquilado mensual
 
+ganado_u = (a_morado(anuncios_m,tipo_usuario) + a_naranja (anuncios_n,tipo_usuario))*30 #ganado en un mes por el usuario
+click_rd = media_rd * 30
+ganado_rd =  r_a_naranja (click_rd,0,tipo_usuario)  * referidos_d
+click_ra = media_ra * 30
+ganado_ra =  r_a_naranja (click_ra,1,tipo_usuario)  * referidos_a
+
+total_ganado = ganado_u + ganado_rd + ganado_ra
+total_ganado = total_ganado - costo_ra * referidos_a
+print (round(total_ganado,3))
