66a39f3d851077bdc1288bd8973f8cbf2bb0f81f
Jose F. Luna <jluna80@gmail.com>
2015-07-02 12:30:16 -0430

    mejora los mensajes al usuario

--- neobux.py
+++ neobux.py
@@ -41,21 +41,39 @@ def r_a_naranja (n=1,tipo_r=0,tipo_u=0):
             click = 0.0100
     return n * click
 
+def ganancia (tipo_usuario, anuncios_n, anuncios_m, media_rd, media_ra, referidos_a, referidos_d , costo_ra):
+    
+    ganado_u = (a_morado(anuncios_m,tipo_usuario) + a_naranja (anuncios_n,tipo_usuario))*30 #ganado en un mes por el usuario
+    click_rd = media_rd * 30
+    ganado_rd =  r_a_naranja (click_rd,0,tipo_usuario)  * referidos_d
+    click_ra = media_ra * 30
+    ganado_ra =  r_a_naranja (click_ra,1,tipo_usuario)  * referidos_a
+
+    total_ganado = ganado_u + ganado_rd + ganado_ra
+    total_ganado = total_ganado - costo_ra * referidos_a
+    return round(total_ganado,3)
+
+def mostrar_ganancia (tipo_usuario, anuncios_n, anuncios_m, media_rd, media_ra, referidos_a, referidos_d , costo_ra):
+
+    if (tipo_usuario == 0):
+        txt = 'standar'
+    else:
+        txt = 'gold'
+    print ("Para un usuario",txt,"con",referidos_d,"referidos directos y",referidos_a,"referidos alquilados")
+    ganado = ganancia (tipo_usuario, anuncios_n, anuncios_m, media_rd, media_ra, referidos_a, referidos_d , costo_ra)
+    print ("podemos estimar que ganara minimo",ganado,"$ al mes")
+    
+
+
 tipo_usuario = 1 # 0 standar 1 gold
 anuncios_n = 4 # anuncios validos naranja para el usuario
 anuncios_m = 19 # anuncios micro y morados del usuario
 media_rd = 18 # media del referido directo
-media_ra = 4 # media del referido alquilado
-referidos_a = 250 # referidos alquilados
+media_ra = 1.7 # media del referido alquilado
 referidos_d = 30 # referidos directo
-costo_ra = 0.2 # referido alquilado mensual
+referidos_a = 1750# referidos alquilados
+costo_ra = 0.26 # referido alquilado mensual
 
-ganado_u = (a_morado(anuncios_m,tipo_usuario) + a_naranja (anuncios_n,tipo_usuario))*30 #ganado en un mes por el usuario
-click_rd = media_rd * 30
-ganado_rd =  r_a_naranja (click_rd,0,tipo_usuario)  * referidos_d
-click_ra = media_ra * 30
-ganado_ra =  r_a_naranja (click_ra,1,tipo_usuario)  * referidos_a
+mostrar_ganancia (tipo_usuario, anuncios_n, anuncios_m, media_rd, media_ra, referidos_a, referidos_d , costo_ra)
 
-total_ganado = ganado_u + ganado_rd + ganado_ra
-total_ganado = total_ganado - costo_ra * referidos_a
-print (round(total_ganado,3))
+    
